4d466912f18011c742b35e5bfd4d09bf34f0e81b
F-ca7 <627955292@qq.com>
2019-03-07 17:32:29 +0800

    更新about

--- app/src/main/java/com/example/fang/activity/AboutUsActivity.java
+++ app/src/main/java/com/example/fang/activity/AboutUsActivity.java
@@ -1,17 +1,47 @@
 package com.example.fang.activity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.sql.Time;
 
 /**
  * Created by FANG on 2018/2/16.
  */
 
 public class AboutUsActivity extends AppCompatActivity {
-
+    private ImageView ivAboutUs;
+    private final int CLICK_COUNT = 5;  //连续点击次数
+    private final int TIME_SPAN = 1000;  //在1000ms以内
+    private long[] mHits = new long[CLICK_COUNT];
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        initView();
     }
+
+    private void initView() {
+        ivAboutUs = findViewById(R.id.iv_bout_us);
+        ivAboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //每点击一次 实现左移一格数据
+                System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
+                //给数组的最后赋当前时钟值
+                mHits[mHits.length - 1] = SystemClock.uptimeMillis();
+                //当0处的值大于当前时间-1000时  证明在1000秒内点击了5次
+                if (mHits[0] > SystemClock.uptimeMillis() - TIME_SPAN) {
+                    Toast.makeText(getApplicationContext(), "一言难尽。。。", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+
 }
